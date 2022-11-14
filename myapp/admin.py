341ece89e7ae53636d1355b6926f5e5e8caef6d9
daxtonkamitaira <daxton.kamitaira@gmail.com>
2022-11-14 01:25:49 +0100

    many to many model

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Person, Group, Membership
+
+admin.site.register(Person)
+admin.site.register(Group)
+admin.site.register(Membership)
